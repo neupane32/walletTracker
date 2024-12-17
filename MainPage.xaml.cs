b2f2c9c96550fe7daa48c12f99b539fb6349a3b3
Aashish Neupane <120081944+neupane32@users.noreply.github.com>
2024-12-17 11:41:51 +0545

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_project
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace dotnet_project
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
