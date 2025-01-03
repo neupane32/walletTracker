02e89c8951e56a962a711af55f3009551cd490fd
Aashish Neupane <120081944+neupane32@users.noreply.github.com>
2025-01-03 15:14:36 +0545

    <coursework update>

---
+++ Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.Models
+{
+    public class Transaction
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice.Models
+{
+    public class User
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; } 
+         public required string Email { get; set; }
+
+        public int Credit { get; set; } = 0;
+
+        public int Debit { get; set; } = 0;
+
+        public int Debt { get; set; } = 0;
+
+        public List<Transaction> Transactions { get; set; } = new List <Transaction>();
+
+    }
+}
